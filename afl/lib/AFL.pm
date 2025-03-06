c11cc6e0ec903717249b257aeae934bd2f2c55f1
stucooper <stuart.cooper@gmail.com>
2025-03-07 00:05:49 +1100

    Perl package AFL.pm

---
+++ afl/lib/AFL.pm
@@ -0,0 +1,13 @@
+package AFL;
+
+# The 18 teams in the 2024 AFL competition, three letter abbreviations
+@Teams = qw ( 
+    ADL BRI CAR
+    COL ESS FRE
+    GEE GCT GWS
+    HAW MEL NOR
+    POR RCH STK
+    SYD WCT WBD
+    );
+
+1;
