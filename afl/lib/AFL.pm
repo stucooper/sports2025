e468b5d513c97aabb846c1ffc3240517664e065e
stucooper <stuart.cooper@gmail.com>
2025-03-07 01:13:35 +1100

    Different abbreviations for Adelaide and Richmond

--- afl/lib/AFL.pm
+++ afl/lib/AFL.pm
@@ -2,11 +2,11 @@ package AFL;
 
 # The 18 teams in the 2024 AFL competition, three letter abbreviations
 @Teams = qw ( 
-    ADL BRI CAR
+    ADE BRI CAR
     COL ESS FRE
     GEE GCT GWS
     HAW MEL NOR
-    POR RCH STK
+    POR RIC STK
     SYD WCT WBD
     );
 
