a83bd53748b908af671e421d90af42680a631715
stucooper <stuart.cooper@gmail.com>
2025-03-07 14:16:57 +1100

    Support module for NRL perl programs

---
+++ nrl/lib/NRL.pm
@@ -0,0 +1,15 @@
+package NRL;
+
+# The 17 teams in the 2024 AFL competition, three letter abbreviations
+@Teams = qw ( 
+    AUK BRI BUL
+    CAN CRO GCT
+    MAN MEL NEW
+    NQL PAR PEN
+    RED ROO SOU
+    STG WTI
+    );
+
+$TEAMCOUNT = 17;
+
+1;
