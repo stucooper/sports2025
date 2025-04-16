612b123b23593bb049724c93e434a411c5bf28ed
stucooper <stuart.cooper@gmail.com>
2025-04-17 01:20:37 +1000

    leading tabs replaced by whitespace

--- afl/bin/ladder.pl
+++ afl/bin/ladder.pl
@@ -70,7 +70,7 @@ foreach (@ladderTeams) {
     $pct = sprintf("%.2f", $pct); # to 2 digits after decimal point
     #       Pos TEAM  P   W   L    D   F    A   %   PTS\n";
     printf("%3s %3s  %2s %2s %2s  %1s %4s %4s %6s %2s\n",
-	     $i, $_, $p, $w, $l,  $d, $f, $a, $pct, $po);
+             $i, $_, $p, $w, $l,  $d, $f, $a, $pct, $po);
     if ($i == 8 ) {
         # we have printed 8 positions of the ladder.. the top 8
         print "=========================================\n";
