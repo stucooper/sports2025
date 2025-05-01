179d71d05b41c65d06acdafb101cdd69c338fa4e
stucooper <stuart.cooper@gmail.com>
2025-05-01 10:33:35 +1000

    Exported sub is_valid_team()

--- afl/lib/AFL.pm
+++ afl/lib/AFL.pm
@@ -1,4 +1,6 @@
 package AFL;
+use Exporter 'import';
+@EXPORT = qw(is_valid_team);
 
 # The 18 teams in the 2025 AFL competition, three letter abbreviations
 @Teams = qw ( 
@@ -9,6 +11,17 @@ package AFL;
     POR RIC STK
     SYD WCT WBD
     );
+
+sub is_valid_team {
+    # return 1 if the single argument is a valid team
+    my ($team) = @_;
+    return 0 if ( length($team) != 3 );
+    foreach ( @Teams ) {
+	return 1 if ( $_ eq $team );
+    }
+    # if we reach here we couldn't find a valid team in the @Teams array
+    return 0;
+}
 
 $TEAMCOUNT = 18;
 
