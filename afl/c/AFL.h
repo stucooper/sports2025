ef7dac3c1bd4a179a24bdf6f603dd778886de6c5
stucooper <stuart.cooper@gmail.com>
2025-06-11 23:35:04 +1000

    Move is_valid_team to its own .c file, teams structure now static

--- afl/c/AFL.h
+++ afl/c/AFL.h
@@ -6,8 +6,8 @@
 
 int is_valid_team(char *team);
 
-/* char *teams[] = { */
-char *teams[TEAMCOUNT] = {
+/* static char *teams[] = { */
+static char *teams[TEAMCOUNT] = {
   "ADE",
   "BRI",
   "CAR",
@@ -30,20 +30,6 @@ char *teams[TEAMCOUNT] = {
 
 };
 
-int is_valid_team (char *team) {
-  int i,valid,len;
-  
-  len = strlen(team);
-  if ( len != 3 ) {
-    return(0);
-  }
-  for (i = 0; i < TEAMCOUNT; i++ ) {
-    if (strcmp(team, teams[i]) == 0) {
-      return(1);
-    }
-  }
-  return(0);
-}
 
 
 
